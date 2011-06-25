91217c0c95ac12b49187e0003ca70758cc7ad492
Ryan Bigg <radarlistener@gmail.com>
2011-06-25 17:20:45 +1000

    Add dummy Rails example application as a base for testing

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'b2974d798ce36e5f069dd1c2ec85d319c0c287b33dffc9a933a303ab2f8f8ae973f78a32615d4464617d8313fe1d7c08884640d232aa285438cfb58ca44692d9'
