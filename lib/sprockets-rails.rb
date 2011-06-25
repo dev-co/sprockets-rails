3ab4446333b1269125f31e2c877137f6b260dbad
Ryan Bigg <radarlistener@gmail.com>
2011-06-25 22:14:54 +1000

    Add Sprockets::Rails::Engine so that we can load the tasks in lib/tasks/assets.rake

--- lib/sprockets-rails.rb
+++ lib/sprockets-rails.rb
@@ -1,5 +1,6 @@
 require 'action_controller/railtie'
 require 'sprockets'
+require 'sprockets/rails/engine'
 require 'sprockets/rails/railtie'
 require 'sprockets/rails/helpers'
 
---
+++ lib/sprockets/rails/engine.rb
@@ -0,0 +1,7 @@
+module Sprockets
+  module Rails
+    class Engine < ::Rails::Engine
+      
+    end
+  end
+end
