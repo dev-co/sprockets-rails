c7f09cc389eb5f0de8f953f12920cef4d2e6d708
Ryan Bigg <radarlistener@gmail.com>
2011-06-29 07:06:30 +1000

    Merge chriseppstein's changes from rails PR#1699

--- lib/sprockets/rails/helpers/asset_tag_helper.rb
+++ lib/sprockets/rails/helpers/asset_tag_helper.rb
@@ -51,12 +51,18 @@ module Sprockets
                 'rel'   => "stylesheet",
                 'type'  => "text/css",
                 'media' => "screen",
-                'href'  => asset_path(source, 'css', body)
+                'href'  => asset_path(source, 'css', body, :request)
               }.merge(options.stringify_keys)
 
               tag 'link', tag_options
             end
           end.join("\n").html_safe
+        end
+
+        def asset_path(source, default_ext = nil, body = false, protocol = :relative)
+          source = source.logical_path if source.respond_to?(:logical_path)
+          path = asset_paths.compute_public_path(source, 'assets', default_ext, protocol)
+          body ? "#{path}?body=1" : path
         end
 
       private
@@ -65,15 +71,14 @@ module Sprockets
             params[:debug_assets] == 'true'
         end
 
-        def asset_path(source, default_ext = nil, body = false)
-          source = source.logical_path if source.respond_to?(:logical_path)
-          path = asset_paths.compute_public_path(source, 'assets', default_ext, true)
-          body ? "#{path}?body=1" : path
-        end
-
         class AssetPaths < ::ActionView::AssetPaths #:nodoc:
-          def compute_public_path(source, dir, ext=nil, include_host=true)
+          def compute_public_path(source, dir, ext=nil, include_host = true, protocol = nil)
             super(source, 'assets', ext, include_host)
+          end
+
+          # Return the filesystem path for the source
+          def compute_source_path(source, ext)
+            asset_for(source, ext)
           end
 
           def asset_for(source, ext)
@@ -104,7 +109,7 @@ module Sprockets
 
           # When included in Sprockets::Context, we need to ask the top-level config as the controller is not available
           def performing_caching?
-            @config ?  @config.perform_caching : ::Rails.application.config.action_controller.perform_caching
+            @config ? @config.perform_caching : ::Rails.application.config.action_controller.perform_caching
           end
         end
       end
--- test/sprockets_helper_test.rb
+++ test/sprockets_helper_test.rb
@@ -26,99 +26,150 @@ class SprocketsHelperTest < ActionView::TestCase
     "http://www.example.com"
   end
 
-  test "asset path" do
-    assert_equal "/assets/logo-d41d8cd98f00b204e9800998ecf8427e.png",
-      asset_path("logo.png")
+  test "asset_path" do
+      assert_equal "/assets/logo-d41d8cd98f00b204e9800998ecf8427e.png",
+        asset_path("logo.png")
+    end
 
-    assert_equal "/images/logo",
-      asset_path("/images/logo")
-    assert_equal "/images/logo.gif",
-      asset_path("/images/logo.gif")
+    test "asset_path with root relative assets" do
+      assert_equal "/images/logo",
+        asset_path("/images/logo")
+      assert_equal "/images/logo.gif",
+        asset_path("/images/logo.gif")
 
-    assert_equal "/dir/audio",
-      asset_path("/dir/audio")
+      assert_equal "/dir/audio",
+        asset_path("/dir/audio")
+    end
 
-    assert_equal "http://www.example.com/video/play",
-      asset_path("http://www.example.com/video/play")
-    assert_equal "http://www.example.com/video/play.mp4",
-      asset_path("http://www.example.com/video/play.mp4")
-  end
+    test "asset_path with absolute urls" do
+      assert_equal "http://www.example.com/video/play",
+        asset_path("http://www.example.com/video/play")
+      assert_equal "http://www.example.com/video/play.mp4",
+        asset_path("http://www.example.com/video/play.mp4")
+    end
 
-  test "asset path with relavtive url root" do
-    @controller.config.relative_url_root = "/collaboration/hieraki"
-    assert_equal "/collaboration/hieraki/images/logo.gif",
-     asset_path("/images/logo.gif")
-  end
+    test "with a simple asset host the url should default to protocol relative" do
+      @controller.config.asset_host = "assets-%d.example.com"
+      assert_match %r{//assets-\d.example.com/assets/logo-[0-9a-f]+.png},
+        asset_path("logo.png")
+    end
 
-  test "javascript path" do
-    assert_equal "/assets/application-89aa68ab245a2f74086f3b0ce57540fa.js",
-      asset_path(:application, "js")
+    test "with a simple asset host the url can be changed to use the request protocol" do
+      @controller.config.asset_host = "assets-%d.example.com"
+      @controller.config.default_asset_host_protocol = :request
+      assert_match %r{http://assets-\d.example.com/assets/logo-[0-9a-f]+.png},
+        asset_path("logo.png")
+    end
 
-    assert_equal "/assets/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js",
-      asset_path("xmlhr", "js")
-    assert_equal "/assets/dir/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js",
-      asset_path("dir/xmlhr.js", "js")
+    test "With a proc asset host that returns no protocol the url should be protocol relative" do
+      @controller.config.asset_host = Proc.new do |asset|
+        "assets-999.example.com"
+      end
+      assert_match %r{//assets-999.example.com/assets/logo-[0-9a-f]+.png},
+        asset_path("logo.png")
+    end
 
-    assert_equal "/dir/xmlhr.js",
-      asset_path("/dir/xmlhr", "js")
+    test "with a proc asset host that returns a protocol the url use it" do
+      @controller.config.asset_host = Proc.new do |asset|
+        "http://assets-999.example.com"
+      end
+      assert_match %r{http://assets-999.example.com/assets/logo-[0-9a-f]+.png},
+        asset_path("logo.png")
+    end
 
-    assert_equal "http://www.example.com/js/xmlhr",
-      asset_path("http://www.example.com/js/xmlhr", "js")
-    assert_equal "http://www.example.com/js/xmlhr.js",
-      asset_path("http://www.example.com/js/xmlhr.js", "js")
-  end
+    test "stylesheets served with a controller in scope can access the request" do
+      config.asset_host = Proc.new do |asset, request|
+        assert_not_nil request
+        "http://assets-666.example.com"
+      end
+      assert_match %r{http://assets-666.example.com/assets/logo-[0-9a-f]+.png},
+        asset_path("logo.png")
+    end
 
-  test "javascript include tag" do
-    assert_equal '<script src="/assets/application-89aa68ab245a2f74086f3b0ce57540fa.js" type="text/javascript"></script>',
-      javascript_include_tag(:application)
+    test "stylesheets served without a controller in do not use asset hosts when the default protocol is :request" do
+      Rails.application.config.action_controller.asset_host = "assets-%d.example.com"
+      Rails.application.config.action_controller.default_asset_host_protocol = :request
+      Rails.application.config.action_controller.perform_caching = true
 
-    assert_equal '<script src="/assets/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js" type="text/javascript"></script>',
-      javascript_include_tag("xmlhr")
-    assert_equal '<script src="/assets/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js" type="text/javascript"></script>',
-      javascript_include_tag("xmlhr.js")
-    assert_equal '<script src="http://www.example.com/xmlhr" type="text/javascript"></script>',
-      javascript_include_tag("http://www.example.com/xmlhr")
+      assert_equal "/assets/logo-d41d8cd98f00b204e9800998ecf8427e.png",
+        asset_path("logo.png")
+    end
 
-    assert_equal "<script src=\"/assets/demo-89aa68ab245a2f74086f3b0ce57540fa.js?body=1\" type=\"text/javascript\"></script>\n<script src=\"/assets/dir/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js?body=1\" type=\"text/javascript\"></script>\n<script src=\"/assets/extra-d41d8cd98f00b204e9800998ecf8427e.js?body=1\" type=\"text/javascript\"></script>\n<script src=\"/assets/omg-f635a55b29904e04e4106d169de7312f.js?body=1\" type=\"text/javascript\"></script>\n<script src=\"/assets/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js?body=1\" type=\"text/javascript\"></script>\n<script src=\"/assets/application-89aa68ab245a2f74086f3b0ce57540fa.js?body=1\" type=\"text/javascript\"></script>",
-      javascript_include_tag(:application, :debug => true)
+    test "asset path with relavtive url root" do
+      @controller.config.relative_url_root = "/collaboration/hieraki"
+      assert_equal "/collaboration/hieraki/images/logo.gif",
+       asset_path("/images/logo.gif")
+    end
 
-    assert_equal  "<script src=\"/assets/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js\" type=\"text/javascript\"></script>\n<script src=\"/assets/extra-d41d8cd98f00b204e9800998ecf8427e.js\" type=\"text/javascript\"></script>",
-      javascript_include_tag("xmlhr", "extra")
-  end
+    test "javascript path" do
+      assert_equal "/assets/application-89aa68ab245a2f74086f3b0ce57540fa.js",
+        asset_path(:application, "js")
 
-  test "stylesheet path" do
-    assert_equal "/assets/application-7ea558a750731502bda5d079e5aab069.css", asset_path(:application, "css")
+      assert_equal "/assets/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js",
+        asset_path("xmlhr", "js")
+      assert_equal "/assets/dir/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js",
+        asset_path("dir/xmlhr.js", "js")
 
-    assert_equal "/assets/style-d41d8cd98f00b204e9800998ecf8427e.css", asset_path("style", "css")
-    assert_equal "/assets/dir/style-d41d8cd98f00b204e9800998ecf8427e.css", asset_path("dir/style.css", "css")
-    assert_equal "/dir/style.css", asset_path("/dir/style.css", "css")
+      assert_equal "/dir/xmlhr.js",
+        asset_path("/dir/xmlhr", "js")
 
-    assert_equal "http://www.example.com/css/style",
-      asset_path("http://www.example.com/css/style", "css")
-    assert_equal "http://www.example.com/css/style.css",
-      asset_path("http://www.example.com/css/style.css", "css")
-  end
+      assert_equal "http://www.example.com/js/xmlhr",
+        asset_path("http://www.example.com/js/xmlhr", "js")
+      assert_equal "http://www.example.com/js/xmlhr.js",
+        asset_path("http://www.example.com/js/xmlhr.js", "js")
+    end
 
-  test "stylesheet link tag" do
-    assert_equal '<link href="/assets/application-7ea558a750731502bda5d079e5aab069.css" media="screen" rel="stylesheet" type="text/css" />',
-      stylesheet_link_tag(:application)
+    test "javascript include tag" do
+      assert_equal '<script src="/assets/application-89aa68ab245a2f74086f3b0ce57540fa.js" type="text/javascript"></script>',
+        javascript_include_tag(:application)
 
-    assert_equal '<link href="/assets/style-d41d8cd98f00b204e9800998ecf8427e.css" media="screen" rel="stylesheet" type="text/css" />',
-      stylesheet_link_tag("style")
-    assert_equal '<link href="/assets/style-d41d8cd98f00b204e9800998ecf8427e.css" media="screen" rel="stylesheet" type="text/css" />',
-      stylesheet_link_tag("style.css")
+      assert_equal '<script src="/assets/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js" type="text/javascript"></script>',
+        javascript_include_tag("xmlhr")
+      assert_equal '<script src="/assets/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js" type="text/javascript"></script>',
+        javascript_include_tag("xmlhr.js")
+      assert_equal '<script src="http://www.example.com/xmlhr" type="text/javascript"></script>',
+        javascript_include_tag("http://www.example.com/xmlhr")
 
-    assert_equal '<link href="http://www.example.com/style.css" media="screen" rel="stylesheet" type="text/css" />',
-      stylesheet_link_tag("http://www.example.com/style.css")
-    assert_equal '<link href="/assets/style-d41d8cd98f00b204e9800998ecf8427e.css" media="all" rel="stylesheet" type="text/css" />',
-      stylesheet_link_tag("style", :media => "all")
-    assert_equal '<link href="/assets/style-d41d8cd98f00b204e9800998ecf8427e.css" media="print" rel="stylesheet" type="text/css" />',
-      stylesheet_link_tag("style", :media => "print")
+      assert_equal "<script src=\"/assets/demo-89aa68ab245a2f74086f3b0ce57540fa.js?body=1\" type=\"text/javascript\"></script>\n<script src=\"/assets/dir/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js?body=1\" type=\"text/javascript\"></script>\n<script src=\"/assets/extra-d41d8cd98f00b204e9800998ecf8427e.js?body=1\" type=\"text/javascript\"></script>\n<script src=\"/assets/omg-f635a55b29904e04e4106d169de7312f.js?body=1\" type=\"text/javascript\"></script>\n<script src=\"/assets/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js?body=1\" type=\"text/javascript\"></script>\n<script src=\"/assets/application-89aa68ab245a2f74086f3b0ce57540fa.js?body=1\" type=\"text/javascript\"></script>",
+        javascript_include_tag(:application, :debug => true)
 
-    assert_equal "<link href=\"/assets/demo-89aa68ab245a2f74086f3b0ce57540fa.js?body=1\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n<link href=\"/assets/dir/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js?body=1\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n<link href=\"/assets/extra-d41d8cd98f00b204e9800998ecf8427e.js?body=1\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n<link href=\"/assets/omg-f635a55b29904e04e4106d169de7312f.js?body=1\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n<link href=\"/assets/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js?body=1\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n<link href=\"/assets/application-7ea558a750731502bda5d079e5aab069.css?body=1\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />",
-      stylesheet_link_tag(:application, :debug => true)
+      assert_equal "<script src=\"/assets/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js\" type=\"text/javascript\"></script>\n<script src=\"/assets/extra-d41d8cd98f00b204e9800998ecf8427e.js\" type=\"text/javascript\"></script>",
+        javascript_include_tag("xmlhr", "extra")
+    end
 
-    assert_equal "<link href=\"/assets/style-d41d8cd98f00b204e9800998ecf8427e.css\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n<link href=\"/assets/extra-d41d8cd98f00b204e9800998ecf8427e.css\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />",
-      stylesheet_link_tag("style", "extra")
-  end
+    test "stylesheet path" do
+      assert_equal "/assets/application-7ea558a750731502bda5d079e5aab069.css", asset_path(:application, "css")
+
+      assert_equal "/assets/style-d41d8cd98f00b204e9800998ecf8427e.css", asset_path("style", "css")
+      assert_equal "/assets/dir/style-d41d8cd98f00b204e9800998ecf8427e.css", asset_path("dir/style.css", "css")
+      assert_equal "/dir/style.css", asset_path("/dir/style.css", "css")
+
+      assert_equal "http://www.example.com/css/style",
+        asset_path("http://www.example.com/css/style", "css")
+      assert_equal "http://www.example.com/css/style.css",
+        asset_path("http://www.example.com/css/style.css", "css")
+    end
+
+    test "stylesheet link tag" do
+      assert_equal '<link href="/assets/application-7ea558a750731502bda5d079e5aab069.css" media="screen" rel="stylesheet" type="text/css" />',
+        stylesheet_link_tag(:application)
+
+      assert_equal '<link href="/assets/style-d41d8cd98f00b204e9800998ecf8427e.css" media="screen" rel="stylesheet" type="text/css" />',
+        stylesheet_link_tag("style")
+      assert_equal '<link href="/assets/style-d41d8cd98f00b204e9800998ecf8427e.css" media="screen" rel="stylesheet" type="text/css" />',
+        stylesheet_link_tag("style.css")
+
+      assert_equal '<link href="http://www.example.com/style.css" media="screen" rel="stylesheet" type="text/css" />',
+        stylesheet_link_tag("http://www.example.com/style.css")
+      assert_equal '<link href="/assets/style-d41d8cd98f00b204e9800998ecf8427e.css" media="all" rel="stylesheet" type="text/css" />',
+        stylesheet_link_tag("style", :media => "all")
+      assert_equal '<link href="/assets/style-d41d8cd98f00b204e9800998ecf8427e.css" media="print" rel="stylesheet" type="text/css" />',
+        stylesheet_link_tag("style", :media => "print")
+
+      assert_equal "<link href=\"/assets/demo-89aa68ab245a2f74086f3b0ce57540fa.js?body=1\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n<link href=\"/assets/dir/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js?body=1\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n<link href=\"/assets/extra-d41d8cd98f00b204e9800998ecf8427e.js?body=1\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n<link href=\"/assets/omg-f635a55b29904e04e4106d169de7312f.js?body=1\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n<link href=\"/assets/xmlhr-d41d8cd98f00b204e9800998ecf8427e.js?body=1\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n<link href=\"/assets/application-7ea558a750731502bda5d079e5aab069.css?body=1\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />",
+        stylesheet_link_tag(:application, :debug => true)
+
+      assert_equal "<link href=\"/assets/style-d41d8cd98f00b204e9800998ecf8427e.css\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />\n<link href=\"/assets/extra-d41d8cd98f00b204e9800998ecf8427e.css\" media=\"screen\" rel=\"stylesheet\" type=\"text/css\" />",
+        stylesheet_link_tag("style", "extra")
+    end
 end
